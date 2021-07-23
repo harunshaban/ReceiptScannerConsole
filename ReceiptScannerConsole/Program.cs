8ef694ae1dc8b1bc9b9a564f36ba0d3742617e85
harunshaban <harun.sabban@gmail.com>
2021-07-23 09:52:26 +0200

    Add project files.

---
+++ ReceiptScannerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReceiptScannerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
